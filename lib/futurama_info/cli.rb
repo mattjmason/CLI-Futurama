7298214e21283407961b37348dd7beaa095442e4
Matt Mason <masonmj10@gmail.com>
2021-01-08 13:06:19 -0500

    error, continue, reset

--- lib/futurama_info/cli.rb
+++ lib/futurama_info/cli.rb
@@ -35,7 +35,7 @@ class Cli
         puts ""
         puts "Welcome to the World of Tomorrow (or just a Futurama CLI app)!".cyan
         puts ""
-        puts "Good News Everyone! You can grab a list of characters from the show, see a variety of quotes, or even learn more about your favorite characters"
+        puts "Good News Everyone! You can grab a list of characters from the show, see a variety of quotes, or even learn more about your favorite characters!"
         puts ""
         puts "To view all Futurama characters from the show, please type 'list characters'."
         puts "To view quotes from Futurama characters, please type 'list quotes'."
@@ -49,14 +49,15 @@ class Cli
 
      if input.downcase == "list quotes"
         list_quotes
-        print_main_menu
+        put_continue 
+        continue?(deeper_choice)
      elsif input.downcase == "list characters"
         list_characters
         more_info
      elsif input.downcase == "exit"
         exit_cli
      else
-        #error for when user messes up
+        print_error 
         print_main_menu
         end
     end
@@ -89,15 +90,15 @@ def more_info
     id = valid_id?(deeper_choice)#confirm that id is valid #select a character by their id
     character = Character.find_by_id(id)#find character in the array of characters so that I can find through their ID 
     character_details(character, id)  #give details about character
-    #continue or exit?
-
+    put_continue #continue or exit?
+    continue?(deeper_choice)
     #start over  
     print_main_menu
-
+    user_main_menu_choice
 end
 
 def deeper_choice
-    gets
+    gets.chomp
 end
 
 def deeper_prompt
@@ -132,37 +133,46 @@ def character_details(character, id)
         puts "Occupation: #{character.homePlanet}"
         puts "Gender: #{character.gender}" #make sure correct 
         puts "Quotes:" 
-            character.sayings.each_with_index do |s, i|
+            character.sayings.each_with_indexy do |s, i|
                 iplusone = i + 1 
             puts "#{iplusone}. #{s} "
             end
         end
 end
 
-
-
-
     def exit_cli
         puts ""
         puts "Thanks for trying out the Planet Express Ship!".cyan
         puts "Hopefully it's the best thing that you ever saw!".cyan 
-        logo_2
         exit
     end
 
 
     def load_data 
+        puts "Waking up from cryogenic sleep...".cyan
+        sleep(1.5)
         quotes_loader = Api.quote
         character_loader = Api.character
+        puts "Doors Open!".cyan
+        sleep(0.5)
     end
 
-def logo_2
-
-    puts "
-
-
-
-   ".cyan
+def print_error
+    puts "That was not a good choice, try again meatbag.".red
 end
 
+def put_continue
+    puts "Would you like to continue? Please enter (y/n)."
+end
+
+def continue?(pick)
+    if pick == "y" 
+        system("clear")
+        print_main_menu
+    else 
+        exit_cli
+        exit
+    end
+    
+end
 end
